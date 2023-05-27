cf533f786f362089790638019e171cb9f3eb679b
iamkaransharmab <karansharmab15072001@gmail.com>
2023-05-27 16:17:10 +0530

    final frontedn

---
+++ src/lib/apiList.js
@@ -0,0 +1,15 @@
+export const server = "https://react-job-portal-backend.vercel.app/";
+
+const apiList = {
+  login: `${server}/auth/login`,
+  signup: `${server}/auth/signup`,
+  uploadResume: `${server}/upload/resume`,
+  uploadProfileImage: `${server}/upload/profile`,
+  jobs: `${server}/api/jobs`,
+  applications: `${server}/api/applications`,
+  rating: `${server}/api/rating`,
+  user: `${server}/api/user`,
+  applicants: `${server}/api/applicants`,
+};
+
+export default apiList;
